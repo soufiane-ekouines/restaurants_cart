5921ab5ab09d2f35d979fd84e2b9f4a06a4e59ee
soufiane-ekouines <soufianeekouines@gmail.com>
2022-09-10 21:07:14 +0100

    footer

--- restaurant/resources/views/layouts/navbars/guest/nav.blade.php
+++ restaurant/resources/views/layouts/navbars/guest/nav.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg position-absolute top-0 z-index-3 my-3 {{ (Request::is('static-sign-up') ? 'w-100 shadow-none  navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4') }}">
   <div class="container-fluid {{ (Request::is('static-sign-up') ? 'container' : 'container-fluid') }}">
     <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 {{ (Request::is('static-sign-up') ? 'text-white' : '') }}" href="{{ url('dashboard') }}">
-      Dashboard soufiane ekouines
+      Dashboard management restaurant
     </a>
     <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon mt-2">
@@ -27,12 +27,12 @@
             </a>
             </li>
         @endif
-        <li class="nav-item">
+        {{-- <li class="nav-item">
           <a class="nav-link me-2" href="{{ auth()->user() ? url('static-sign-up') : url('register') }}">
             <i class="fas fa-user-circle opacity-6 me-1 {{ (Request::is('static-sign-up') ? '' : 'text-dark') }}"></i>
             Sign Up
           </a>
-        </li>
+        </li> --}}
         <li class="nav-item">
           <a class="nav-link me-2" href="{{ auth()->user() ? url('static-sign-in') : url('login') }}">
             <i class="fas fa-key opacity-6 me-1 {{ (Request::is('static-sign-up') ? '' : 'text-dark') }}"></i>
